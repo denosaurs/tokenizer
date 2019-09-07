4151c239b80edf8237b3fb03666d280963eecb30
eliassjogreen <eliassjogreen1@gmail.com>
2019-09-07 22:11:00 +0200

    Added tests for Iterable

--- test.ts
+++ test.ts
@@ -4,10 +4,18 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { Rule, Token, Tokenizer } from "./mod.ts";
 
 test(function matchesSingleRegex() {
-    const tokenizer = new Tokenizer("0123456789", [ { name: "DIGITS", pattern: /\d+/ } ]);
+    const tokenizer = new Tokenizer("0123456789", [
+        { name: "DIGITS", pattern: /\d+/ }
+    ]);
 
-    assertEquals(tokenizer.next(), { done: false, value: { name: "DIGITS", value: "0123456789" } });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "DIGITS", value: "0123456789" }
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
 });
 
 test(function matchesMultipleRegex() {
@@ -16,17 +24,34 @@ test(function matchesMultipleRegex() {
         { name: "SPACE", pattern: / / }
     ]);
 
-    assertEquals(tokenizer.next(), { done: false, value: { name: "DIGITS", value: "0123456789" } });
-    assertEquals(tokenizer.next(), { done: false, value: { name: "SPACE", value: " " } });
-    assertEquals(tokenizer.next(), { done: false, value: { name: "DIGITS", value: "0123456789" } });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "DIGITS", value: "0123456789" }
+    });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "SPACE", value: " " }
+    });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "DIGITS", value: "0123456789" }
+    });
     assertEquals(tokenizer.next(), { done: true, value: undefined });
 });
 
 test(function matchesSingleString() {
-    const tokenizer = new Tokenizer("0123456789", [ { name: "DIGITS", pattern: "0123456789" } ]);
+    const tokenizer = new Tokenizer("0123456789", [
+        { name: "DIGITS", pattern: "0123456789" }
+    ]);
 
-    assertEquals(tokenizer.next(), { done: false, value: { name: "DIGITS", value: "0123456789" } });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "DIGITS", value: "0123456789" }
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
 });
 
 test(function matchesMultipleString() {
@@ -35,17 +60,37 @@ test(function matchesMultipleString() {
         { name: "SPACE", pattern: " " }
     ]);
 
-    assertEquals(tokenizer.next(), { done: false, value: { name: "DIGITS", value: "0123456789" } });
-    assertEquals(tokenizer.next(), { done: false, value: { name: "SPACE", value: " " } });
-    assertEquals(tokenizer.next(), { done: false, value: { name: "DIGITS", value: "0123456789" } });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "DIGITS", value: "0123456789" }
+    });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "SPACE", value: " " }
+    });
+    assertEquals(tokenizer.next(), {
+        done: false,
+        value: { name: "DIGITS", value: "0123456789" }
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
 });
 
 test(function ignoresSingleRegex() {
-    const tokenizer = new Tokenizer("0123456789", [ { name: "", pattern: /\d+/ } ]);
+    const tokenizer = new Tokenizer("0123456789", [
+        { name: "", pattern: /\d+/ }
+    ]);
 
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
 });
 
 test(function ignoresMultipleRegex() {
@@ -53,18 +98,38 @@ test(function ignoresMultipleRegex() {
         { name: "", pattern: /\d+/ },
         { name: "", pattern: / / }
     ]);
-    
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
 });
 
 test(function ignoresSingleString() {
-    const tokenizer = new Tokenizer("0123456789", [ { name: "", pattern: "0123456789" } ]);
+    const tokenizer = new Tokenizer("0123456789", [
+        { name: "", pattern: "0123456789" }
+    ]);
 
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
 });
 
 test(function ignoresMultipleString() {
@@ -72,11 +137,39 @@ test(function ignoresMultipleString() {
         { name: "", pattern: "0123456789" },
         { name: "", pattern: " " }
     ]);
-    
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
-    assertEquals(tokenizer.next(), { done: true, value: undefined });
+
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+    assertEquals(tokenizer.next(), {
+        done: true,
+        value: undefined
+    });
+});
+
+test(function testIterable() {
+    const tokenizer = new Tokenizer("0123456789 0123456789", [
+        { name: "DIGITS", pattern: /\d+/ },
+        { name: "SPACE", pattern: / / }
+    ]);
+
+    assertEquals(
+        [...tokenizer],
+        [
+            { name: "DIGITS", value: "0123456789" },
+            { name: "SPACE", value: " " },
+            { name: "DIGITS", value: "0123456789" }
+        ]
+    );
 });
 
 runTests();
